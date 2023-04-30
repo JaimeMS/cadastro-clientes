e70d70ca76def52cbeeb0f569a63479d9eb301f9
Jaime <santos.jaimemendes91@gmail.com>
2023-04-30 13:26:05 -0300

    CONFIGURAÇÃO DO OAUTH 2.0 - BASIC
    - Adicionar as dependências no arquivo pom.xml
    <dependency>
    	<groupId>org.springframework.boot</groupId>
    	<artifactId>spring-boot-starter-security</artifactId>
    </dependency>
    <dependency>
    	<groupId>org.springframework.security.oauth.boot</groupId>
    	<artifactId>spring-security-oauth2-autoconfigure</artifactId>
    	<version>2.0.1.RELEASE</version>
    </dependency>
    <dependency>
    	<groupId>org.springframework.security</groupId>
    	<artifactId>spring-security-jwt</artifactId>
    	<version>1.1.0.RELEASE</version>
    </dependency>
    
    - informar o modo do escopo no arquivo application.properties
    security.oauth2.client.scope=password
    
    - Criar uma classe de configuração SecurityConfig e extender WebSecurityConfigurerAdapter
    	- adicionar as seguintes anotações
    		@EnableWebSecurity
    		@EnableResourceServer
    		@EnableAuthorizationServer

---
+++ src/main/java/io/github/jaimems/clientes/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package io.github.jaimems.clientes.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+
+@EnableWebSecurity
+@EnableResourceServer
+@EnableAuthorizationServer
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+}
---
+++ src/main/java/io/github/jaimems/clientes/rest/UsuarioController.java
@@ -0,0 +1,30 @@
+package io.github.jaimems.clientes.rest;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.jaimems.clientes.model.entity.Usuario;
+import io.github.jaimems.clientes.model.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+	
+	private final UsuarioRepository repository;
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public void salvar(@RequestBody @Valid Usuario usuario) {
+		repository.save(usuario);
+	}
+	
+	
+}
