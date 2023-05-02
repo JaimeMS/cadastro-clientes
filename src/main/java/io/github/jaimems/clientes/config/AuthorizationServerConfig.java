5c9adcec85685b68be3d62b52cda543d0b406e38
Jaime <santos.jaimemendes91@gmail.com>
2023-05-02 20:13:18 -0300

    CONFIGURADO UM USUARIO ESTATICO PARA TESTE E ADICIONADA DUAS CLASSES

---
+++ src/main/java/io/github/jaimems/clientes/config/AuthorizationServerConfig.java
@@ -0,0 +1,5 @@
+package io.github.jaimems.clientes.config;
+
+public class AuthorizationServerConfig {
+
+}
---
+++ src/main/java/io/github/jaimems/clientes/config/ResourceServerConfig.java
@@ -0,0 +1,5 @@
+package io.github.jaimems.clientes.config;
+
+class ResourceServerConfig {
+
+}
